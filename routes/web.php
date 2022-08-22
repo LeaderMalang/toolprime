0f84e89875a809e27a6d30c53877a9bbed423a5f
hassan ali <hassanali5120@gmail.com>
2022-08-23 00:53:01 +0500

    aded routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,63 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('admin_panel.index');
+});
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth', 'permission']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+
+        /**
+         * User Routes
+         */
+        Route::group(['prefix' => 'users'], function() {
+            Route::get('/', 'UsersController@index')->name('users.index');
+            Route::get('/create', 'UsersController@create')->name('users.create');
+            Route::post('/create', 'UsersController@store')->name('users.store');
+            Route::get('/{user}/show', 'UsersController@show')->name('users.show');
+            Route::get('/{user}/edit', 'UsersController@edit')->name('users.edit');
+            Route::patch('/{user}/update', 'UsersController@update')->name('users.update');
+            Route::delete('/{user}/delete', 'UsersController@destroy')->name('users.destroy');
+        });
+
+        /**
+         * User Routes
+         */
+        Route::group(['prefix' => 'packages'], function() {
+            Route::get('/', 'PackagesController@index')->name('packages.index');
+            Route::get('/create', 'PackagesController@create')->name('packages.create');
+            Route::post('/create', 'PackagesController@store')->name('packages.store');
+            Route::get('/{package}/show', 'PackagesController@show')->name('packages.show');
+            Route::get('/{package}/edit', 'PackagesController@edit')->name('package.edit');
+            Route::patch('/{package}/update', 'PackagesController@update')->name('packages.update');
+            Route::delete('/{package}/delete', 'PackagesController@destroy')->name('package.destroy');
+        });
+
+        Route::resource('roles', RolesController::class);
+        Route::resource('permissions', PermissionsController::class);
+    });
 });
